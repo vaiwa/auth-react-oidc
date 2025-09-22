67d24b77e448187914fa5e2203b806e95cd187b4
Jan Návrat <jan.navrat@ceskatelevize.cz>
2025-09-22 20:35:39 +0200

    fix logout redirect

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,19 +5,20 @@ export const Nav = () => {
   const auth = useAuth()
   console.info('USER', auth.user)
 
+  const handleLogout = () => {
+    if (import.meta.env.VITE_OIDC_USE_KEYCLOAK === 'true') {
+      return auth.signoutSilent()
+    }
+    auth.signoutRedirect({ post_logout_redirect_uri: 'http://localhost:3100/profil/odhlaseni/' })
+  }
+
   return (
     <nav className="nav">
       <Link to="/">Home</Link>
       <Link to="/unsecured">Unsecured</Link>
       <Link to="/secured">Secured</Link>
       {!auth.isAuthenticated && <button onClick={() => auth.signinRedirect()}>Login</button>}
-      {auth.isAuthenticated && (
-        <button
-          onClick={() => auth.signoutRedirect({ post_logout_redirect_uri: 'http://localhost:3100/profil/odhlaseni/' })}
-        >
-          Logout {auth.user?.profile.name}
-        </button>
-      )}
+      {auth.isAuthenticated && <button onClick={handleLogout}>Logout {auth.user?.profile.name}</button>}
     </nav>
   )
 }
